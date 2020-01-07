585b1d03867ba4286023eaa2fe2034c97227639b
putuvandi <59043955+putuvandi@users.noreply.github.com>
2020-01-07 11:32:54 +0700

    My commit message

---
+++ app/src/main/java/com/example/cobacrud/ListView.java
@@ -0,0 +1,14 @@
+package com.example.cobacrud;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.list_item);
+    }
+}
---
+++ app/src/main/java/com/example/cobacrud/RequestHandler.java
@@ -0,0 +1,124 @@
+package com.example.cobacrud;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.net.ssl.HttpsURLConnection;
+
+public class RequestHandler {
+
+    // untuk mengirim httpPostRequest
+    public String sendPostRequest(String requestURL,
+                                  HashMap<String, String> postDataParams) {
+
+        URL url;
+
+        // Objek StringBuilder utk menyimpan pesan diambil dari server
+        StringBuilder sb = new StringBuilder();
+
+        try {
+            // inisialisasi url
+            url = new URL(requestURL);
+
+            // membuat koneksi HttpURLConnection
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+            // konfigurasi koneksi
+            conn.setReadTimeout(15000);
+            conn.setConnectTimeout(15000);
+            conn.setRequestMethod("POST");
+            conn.setDoInput(true);
+            conn.setDoOutput(true);
+
+            // membuat keluaran stream
+            OutputStream os = conn.getOutputStream();
+
+            // menulis untuk parameter permintaan
+            BufferedWriter writer = new BufferedWriter(
+                    new OutputStreamWriter(os, "UTF-8"));
+            writer.write(getPostDataString(postDataParams));
+
+            writer.flush();
+            writer.close();
+            os.close();
+
+            int responseCode = conn.getResponseCode();
+            if (responseCode == HttpsURLConnection.HTTP_OK) {
+                BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                sb = new StringBuilder();
+                String response;
+                while ((response = br.readLine()) != null) {
+                    sb.append(response);
+                }
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return sb.toString();
+    }
+
+    public String sendGetRequest(String requestURL) {
+        StringBuilder sb = new StringBuilder();
+
+        try {
+            URL url = new URL(requestURL);
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            BufferedReader br = new BufferedReader(new InputStreamReader(con.getInputStream()));
+
+            String s;
+            while ((s = br.readLine()) != null) {
+                sb.append(s+"\n");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return sb.toString();
+    }
+
+    public String sendGetRequestParam(String requestURL, String id) {
+        StringBuilder sb = new StringBuilder();
+
+        try {
+            URL url = new URL(requestURL+id);
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            BufferedReader br = new BufferedReader(new InputStreamReader(con.getInputStream()));
+
+            String s;
+            while ((s = br.readLine()) != null) {
+                sb.append(s+"\n");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return sb.toString();
+    }
+
+    public String getPostDataString(HashMap<String, String> params) throws UnsupportedEncodingException {
+        StringBuilder result = new StringBuilder();
+
+        boolean first = true;
+
+        for (Map.Entry<String, String> entry : params.entrySet()) {
+            if (first) {
+                first = false;
+            } else {
+                result.append("&");
+            }
+            result.append(URLEncoder.encode(entry.getKey(), "UTF-8"));
+            result.append("=");
+            result.append(URLEncoder.encode(entry.getValue(), "UTF-8"));
+        }
+        return result.toString();
+    }
+
+}
